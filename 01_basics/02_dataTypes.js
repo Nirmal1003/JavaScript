39381b44a24a596f5967e4c9c54d6f03a1f439d8
Nirmal1003 <86091251+Nirmal1003@users.noreply.github.com>
2025-08-30 06:50:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";   // it treats all JS code as newer version (old version did not use class, modules, arrow function, etc)
+
+// alert(3+3) //we are using node js, not browser
+
+console.log(3
+    +
+    3)    // this is not the good practice, affects the code readability
+
+console.log("Nirmal")
+
+
+let name = "Nirmal"    // string datatype
+let age = 23           // number
+let isLoggedIn =  false     // boolean
+let state;
+
+// number => 2 to power 53      it is the range of number tha we get in memory address
+
+// bigint => if no. is too big then only we use bigint especially in trading, stockmarket and in very large websites like reddit, facebook, etc
+
+// string => "", ''
+// boolean => true/false
+// null => standalone  value ( when we want an empty value)
+// undefined => value is still not assigned
+// symbol => to find uniqueness symbol is used
+
+
+// object
+
+
+console.log(typeof null);  //output of this is => object
+console.log(typeof undefined); // output of this is => undefined (because it is type in javascript)
