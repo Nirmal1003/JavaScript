93d750c9fb2b9559df6a6ee55e7f36e716c2d6e7
Nirmal1003 <86091251+Nirmal1003@users.noreply.github.com>
2025-09-02 06:19:20 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,15 @@
 let score = "Nirmal"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let ValueInNumber = Number(score)
 
-console.log(typeof ValueInNumber);
+// console.log(typeof ValueInNumber);
 
-console.log(ValueInNumber);    //NaN is a special type
+// console.log(ValueInNumber);    //NaN is a special type
 
+/*
 // "33" =>33
 // "33abc" => NaN
 // true => 1, false => 0
@@ -16,20 +17,65 @@ console.log(ValueInNumber);    //NaN is a special type
 // undefined => undefined
 // "nirmal" => NaN (because string cant be converted to nuumber)
 
+*/
+
 let isLoggedIn = "nirmal"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// "1" => true, 0 => false
-// " " => false
-// "nirmal" => true
-
+/* "1" => true, 0 => false
+   " " => false
+   "nirmal" => true
+*/
 
 let someNumber = 33
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************************** Operations ********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " nirmal"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+2+"2");
+// console.log(1+"2"+2);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1= num2 = num3 =2+2    // not preferred , because it hampers the code readability
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter)
+
+let gameCounterr = 100
+++gameCounterr;
+console.log(gameCounterr);
